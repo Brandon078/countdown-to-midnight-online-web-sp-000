4727072b98551f1f3349a27efb197e4b8231ed89
brandon moro <brandon_078@hotmail.com>
2019-11-12 01:15:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(count)
-  count= 10
-  while count > 0
-    count -= 1
+  count= 0
+  while count > -10
+    count += 1
     puts "#{count} SECOND(S)!"
     Happy New Year
 end
